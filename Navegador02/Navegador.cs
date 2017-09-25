8e99a0b206367b7cf4828ccae36b04cd3cd0eeb2
Cdsr99 <31573978+Cdsr99@users.noreply.github.com>
2017-09-25 00:17:43 -0300

    testão

---
+++ Navegador02/Navegador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador02
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            String link = "www.artic9980.wixsite.com/artic7789";
+            webBrowser1.Navigate(link);
+           
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pesquisa();
+            textBox1.Text = "";
+        }
+        public void pesquisa()
+        {
+            webBrowser1.Navigate(textBox1.Text);
+           
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+    }
+}
